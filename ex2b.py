d2004d4dd4db8ab29806b956f8ffb84b945eb8d6
ricardo <750372880@qq.com>
2018-07-28 10:12:20 +0800

    del

---
+++ ex2b.py
@@ -0,0 +1,8 @@
+pi=3.14
+import numpy as np
+np.pi
+r=6378
+C=pi*r*2
+print 'the circumference is = ',C
+print '21/4?',21/4
+print '21.0/4.0?',21.0/4.0
---
+++ ex2c.py
@@ -0,0 +1,6 @@
+cars=100
+drivers=30
+passengers=85
+print 'There are ',cars,'cars available.'
+print 'There are only ',drivers,'drivers available.'
+print 'Each driver needs to drive',passengers/drivers+1,'passengers.'
